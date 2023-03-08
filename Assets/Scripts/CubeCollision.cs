ac0d2543813b35b0f9769399bccdc22680abb344
Fzkkkc <ivan.boris.04@mail.ru>
2023-03-08 13:48:33 +0300

    commit 08.08.23
    Оптимизация игры

--- Assets/Scripts/CubeCollision.cs
+++ Assets/Scripts/CubeCollision.cs
@@ -3,51 +3,52 @@ using UnityEngine;
 public class CubeCollision : MonoBehaviour
 {
     private Cube _cube;
+    private Rigidbody _cubeRigidbody;
 
-    private void Awake() => 
+    private static readonly int MAX_CUBE_NUMBER = 4096 ;
+    private static readonly float EXPLOSION_FORCE = 400f;
+    private static readonly float EXPLOSION_RADIUS = 1.5f;
+    private static readonly float PUSH_FORCE = 2.5f;
+
+    private void Awake()
+    {
         _cube = GetComponent<Cube>();
+        _cubeRigidbody = GetComponent<Rigidbody>();
+    }
 
     private void OnCollisionEnter(Collision other)
     {
         Cube otherCube = other.gameObject.GetComponent<Cube>();
 
-        if (otherCube != null && _cube.CubeID > otherCube.CubeID)
+        if (otherCube == null || _cube.CubeID <= otherCube.CubeID || _cube.CubeNumber != otherCube.CubeNumber)
         {
-            if (_cube.CubeNumber == otherCube.CubeNumber)
+            return;
+        }
+
+        Vector3 contactPoint = other.contacts[0].point;
+
+        if (otherCube.CubeNumber < MAX_CUBE_NUMBER)
+        {
+            Cube newCube = CubeSpawner.Instance.Spawn(_cube.CubeNumber * 2, contactPoint + Vector3.up * 1.6f);
+
+            newCube.CubeRigidbody.AddForce(new Vector3(0, 0.3f, 1f) * PUSH_FORCE, ForceMode.Impulse);
+            newCube.CubeRigidbody.AddTorque(Vector3.one * Random.Range(-20f, 20f));
+        }
+
+        Collider[] surroundedCubes = Physics.OverlapSphere(contactPoint, 2f);
+
+        foreach (Collider collider in surroundedCubes)
+        {
+            Rigidbody rigidbody = collider.attachedRigidbody;
+            if (rigidbody != null)
             {
-                Vector3 contactPoint = other.contacts[0].point;
-
-                if (otherCube.CubeNumber < CubeSpawner.Instance.MaxCubeNumber)
-                {
-                    Cube newCube = CubeSpawner.Instance.Spawn(
-                        _cube.CubeNumber * 2,
-                        contactPoint + Vector3.up * 1.6f);
-
-                    float pushForce = 2.5f;
-                    newCube.CubeRigidbody.AddForce(
-                        new Vector3(0, 0.3f, 1f) * 
-                        pushForce, ForceMode.Impulse);
-
-                    float randomValue = Random.Range(-20f, 20f);
-                    Vector3 randomDirection = Vector3.one * randomValue;
-                    newCube.CubeRigidbody.AddTorque(randomDirection);
-                }
-
-                Collider[] surroundedCubes = Physics.OverlapSphere(contactPoint, 2f);
-                float explosionForce = 400f;
-                float explosionRadius = 1.5f;
-
-                foreach (Collider collider in surroundedCubes)
-                {
-                    if (collider.attachedRigidbody != null)
-                        collider.attachedRigidbody.AddExplosionForce(explosionForce, contactPoint, explosionRadius);
-                }
-                
-                FX.Instance.PlayCubeExplosionFX(contactPoint, _cube.CubeColor);
-                
-                CubeSpawner.Instance.DestroyCube(_cube);
-                CubeSpawner.Instance.DestroyCube(otherCube);
+                rigidbody.AddExplosionForce(EXPLOSION_FORCE, contactPoint, EXPLOSION_RADIUS);
             }
         }
+
+        FX.Instance.PlayCubeExplosionFX(contactPoint, _cube.CubeColor);
+
+        CubeSpawner.Instance.DestroyCube(_cube);
+        CubeSpawner.Instance.DestroyCube(otherCube);
     }
 }
